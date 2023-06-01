6cb16944bf762fb8a78a911b257f0e395b9f36fc
igor <kishm2013@mail.ru>
2023-06-01 16:40:02 +0300

    дз 4

---
+++ dz_4/Node.java
@@ -0,0 +1,12 @@
+package dz_4;
+
+public class Node <K, V>{
+    K key;
+    V value;
+    Node<K, V> nextnode;
+
+    public Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
